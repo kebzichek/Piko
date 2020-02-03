13fdd59e6573d0a5df6f2d1fffec9b8fcac15fdb
DomcaSuzi <dominio122@gmail.com>
2020-02-04 00:07:12 +0100

    funkci mail

--- ajax.php
+++ ajax.php
@@ -1,8 +1,7 @@
 <?php 
 $_POST = json_decode(file_get_contents("php://input"),true);
 
-
-if (isset($_POST["type"]) && $_POST["type"] == "email"){
+if (isset($_POST["type"]) && $_POST["type"] == "Contact"){
     send_mail($_POST["email"], $_POST["name"], $_POST["phone"], $_POST["message"]);
 }
 
@@ -13,5 +12,6 @@ function send_mail($from, $name, $phone, $message){
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     
     mail("dominio@tvorbamap.cz", "Kontaktní formulář | Piko ", $message_formated, $headers);
+    echo "ok";
 }
 ?>
--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const state = () => ({
-    ajaxfile: "http://localhost:8888/piko/ajaxfile.php"
+    ajaxfile: "ajax.php"
 })
